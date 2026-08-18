494f59c4540f0816ecb6c9d0d533b2e4718cfbb5
Alexandre GV. <contact@alexandregv.fr>
2026-08-19 01:53:58 +0200

    build(nix): add nix flake with Go devShell

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A basic flake with Go devShell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      forAllSystems = f: nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system: f (import nixpkgs { inherit system; }));
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            gopls
+            gotools
+            golangci-lint
+            tree-sitter
+            tree-sitter-grammars.tree-sitter-go
+          ];
+        };
+      });
+    };
+}
