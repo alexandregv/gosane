1906c0d75d001cf2e42c590a6e56af8942f89690
Alexandre GV. <contact@alexandregv.fr>
2026-08-19 01:58:17 +0200

    feat(nogenerics): add nogenerics

---
+++ nogenerics/nogenerics.go
@@ -0,0 +1,40 @@
+package nogenerics
+
+import (
+	"go/ast"
+
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/ast/inspector"
+)
+
+func Analyzer() *analysis.Analyzer {
+	return &analysis.Analyzer{
+		Name:     "nogenerics",
+		Doc:      "Forbids the use of generics (type parameters)",
+		Run:      run,
+		Requires: []*analysis.Analyzer{inspect.Analyzer},
+	}
+}
+
+func run(pass *analysis.Pass) (any, error) {
+	insp, ok := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+	if !ok {
+		return nil, nil
+	}
+
+	insp.Preorder([]ast.Node{(*ast.FuncType)(nil), (*ast.TypeSpec)(nil)}, func(node ast.Node) {
+		switch n := node.(type) {
+		case *ast.FuncType:
+			if n.TypeParams != nil && n.TypeParams.NumFields() > 0 {
+				pass.Reportf(n.Pos(), "generics are not allowed (type parameters in function)")
+			}
+		case *ast.TypeSpec:
+			if n.TypeParams != nil && n.TypeParams.NumFields() > 0 {
+				pass.Reportf(n.Pos(), "generics are not allowed (type parameters in type declaration)")
+			}
+		}
+	})
+
+	return nil, nil
+}
