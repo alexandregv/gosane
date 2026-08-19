2dbfd518c85b8b66da21af98fb8f05d3718d9b5a
Alexandre GV. <contact@alexandregv.fr>
2026-08-19 02:55:20 +0200

    feat(gochecknoinits): reimplement and export

---
+++ gochecknoinits/gochecknoinits.go
@@ -0,0 +1,38 @@
+// Package gochecknoinits reimplements gochecknoinits but using the go/analysis standard.
+// golangci-lint also [reimplements it but doesn't export it].
+//
+// [reimplements it but doesn't export it]: https://github.com/golangci/golangci-lint/blob/main/pkg/golinters/gochecknoinits/gochecknoinits.go
+package gochecknoinits
+
+import (
+	"go/ast"
+
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/ast/inspector"
+)
+
+func Analyzer() *analysis.Analyzer {
+	return &analysis.Analyzer{
+		Name:     "gochecknoinits",
+		Doc:      "Checks that no init functions are present in Go code",
+		Run:      run,
+		Requires: []*analysis.Analyzer{inspect.Analyzer},
+	}
+}
+
+func run(pass *analysis.Pass) (any, error) {
+	insp, ok := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+	if !ok {
+		return nil, nil
+	}
+
+	insp.Preorder([]ast.Node{(*ast.FuncDecl)(nil)}, func(node ast.Node) {
+		funcDecl := node.(*ast.FuncDecl)
+		if funcDecl.Name.Name == "init" && funcDecl.Recv == nil {
+			pass.Reportf(funcDecl.Pos(), "init functions are not allowed")
+		}
+	})
+
+	return nil, nil
+}
