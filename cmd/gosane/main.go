58157c8239eeccd1500e32c5809bbfc8dcaf2263
Alexandre GV. <contact@alexandregv.fr>
2026-08-19 02:21:15 +0200

    feat(cmd/gosane): add noinlineerr/checknoglobals

--- cmd/gosane/main.go
+++ cmd/gosane/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"golang.org/x/tools/go/analysis/multichecker"
 
+	"4d63.com/gochecknoglobals/checknoglobals"
+	"github.com/AlwxSin/noinlineerr"
+
 	"github.com/alexandregv/gosane/nogenerics"
 	"github.com/alexandregv/gosane/noiter"
 )
@@ -11,5 +14,7 @@ func main() {
 	multichecker.Main(
 		noiter.Analyzer(),
 		nogenerics.Analyzer(),
+		noinlineerr.NewAnalyzer(),
+		checknoglobals.Analyzer(),
 	)
 }
